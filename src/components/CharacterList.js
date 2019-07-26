95213a1486683b993a47be3f47ccc16cc1ebf597
masonkarsevar <masonkarsevar@gmail.com>
2019-07-26 08:09:01 -0700

    axios installed and useState added to CharacterList.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
   }, [])
 
   return <section className='character-list grid-view'>
